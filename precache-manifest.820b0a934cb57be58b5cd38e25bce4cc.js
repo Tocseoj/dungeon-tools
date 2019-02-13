5d40cab554bd37c391277244e8ccf81eb0fe772b
Tocseoj <marsha87@purdue.edu>
2019-02-13 09:49:41 -0500

    Updates

---
+++ precache-manifest.820b0a934cb57be58b5cd38e25bce4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32972fba1c5259d34aa",
+    "url": "/dungeon-tools/static/css/main.c4015a1e.chunk.css"
+  },
+  {
+    "revision": "c32972fba1c5259d34aa",
+    "url": "/dungeon-tools/static/js/main.c32972fb.chunk.js"
+  },
+  {
+    "revision": "249aaa0d6686eafb570b",
+    "url": "/dungeon-tools/static/js/runtime~main.249aaa0d.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/dungeon-tools/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dungeon-tools/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "61162a12659c90ff07acaf5a04beb774",
+    "url": "/dungeon-tools/index.html"
+  }
+];
