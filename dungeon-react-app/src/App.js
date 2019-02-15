4594c61d7c96b6fdda1520867fbbb7d966af9964
Tocseoj <marsha87@purdue.edu>
2019-02-15 10:42:44 -0500

    testing

--- dungeon-react-app/src/App.js
+++ dungeon-react-app/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TurnTracker from './TurnTracker';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/*<img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -18,7 +20,10 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a>*/}
+
+          <TurnTracker />
+
         </header>
       </div>
     );
---
+++ dungeon-react-app/src/TurnTracker.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import './TurnTracker.css';
+
+class TurnTracker extends Component {
+  state = {
+    combatants: [
+      {
+        name: "Waorjh",
+        type: "player",
+        order: 0,
+      },
+      {
+        name: "Bingo",
+        type: "player",
+        order: 1,
+      },
+      {
+        name: "Michelle",
+        type: "player",
+        order: 2,
+      },
+      {
+        name: "Be'urf",
+        type: "player",
+        order: 3,
+      },
+      {
+        name: "Meridian",
+        type: "player",
+        order: 4,
+      },
+      {
+        name: "Jeff",
+        type: "player",
+        order: 5,
+      },
+      {
+        name: "Lucien",
+        type: "player",
+        order: 6,
+      },
+      {
+        name: "Troglodyte(s)",
+        type: "enemy",
+        order: 7,
+      },
+    ],
+  }
+
+  onClickAnimate = () => {
+    this.nextInOrder();
+  }
+
+  nextInOrder = () => {
+    const len = this.state.combatants.length;
+    this.setState({
+      combatants: this.state.combatants.map((item, index) => ({
+        name: item.name,
+        type: item.type,
+        order: (item.order === 0 ? len - 1 : item.order - 1),
+      })),
+    });
+  }
+
+  render() {
+    const {combatants} = this.state;
+
+    return (
+      <div id="TurnTracker" onClick={this.onClickAnimate} className="TurnTracker">
+        {combatants.map((item, index) => (
+          <p  key={item.name} 
+              className={[item.type, "order-" + item.order, item.order > 2 ? "hidden" : ""].join(" ")}
+              style={{order: item.order}}
+          >
+            {item.name}
+          </p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default TurnTracker;
