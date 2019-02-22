976be2b6d14eebf4e2859d701a949d47aa77fd72
Tocseoj <marsha87@purdue.edu>
2019-02-22 00:12:02 -0500

    cleanup

--- dungeon-react-app/src/components/SpotifyPlaylists.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import api from '../util/api'
-
-const SpotifyPlaylists = () => {
-	console.log(api.getDndPlaylists());
-	return <p>Playlists</p>
-}
-
-export default SpotifyPlaylists;
